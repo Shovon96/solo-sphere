61fd204da31408942e7b012e931ac058d775bd5f
Shovon96 <fakhruddinshovon@gmail.com>
2024-10-05 00:11:13 +0600

    [Page add] login and register page added

--- client/src/routers/Router.jsx
+++ client/src/routers/Router.jsx
@@ -1,11 +1,28 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../layouts/main";
+import Home from "../pages/Home";
+import Login from "../pages/Authentication/Login";
+import Registration from "../pages/Authentication/Registation";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/login',
+          element: <Login />
+        },
+        {
+          path: '/registration',
+          element: <Registration />
+        }
+      ]
     },
   ]);
 
