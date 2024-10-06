f0e48c7c1ff9caf59c2c91191d541c68a5ff2112
Shovon96 <fakhruddinshovon@gmail.com>
2024-10-07 00:12:02 +0600

    [add and connect] create server site and connect to the mongobd

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config;
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+const corsOptions = {
+    origin: ['http://localhost:5000'],
+    credential: true,
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.riywk8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Hello from solo Sphere......")
+})
+
+
+
+app.listen(port, console.log(`server is on ${port}`))
